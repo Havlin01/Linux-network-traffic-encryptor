89e9200a91f5207ec6997363615042306782c462
Jan Havlin <241030@vutbr.cz>
2024-03-09 14:16:20 +0100

    fix

--- client_cert.sh
+++ client_cert.sh
@@ -2,19 +2,19 @@
 
 
 # Set the paths
-CLIENT_KEY="client.key"
-CLIENT_CSR="client.csr"
-CLIENT_CERT="client.crt"
+CLIENT_KEY="server_key.pem"
+CLIENT_CSR="server_csr.pem"
+CLIENT_CERT="server_cert.pem"
 CA_KEY="ca.key"
 CA_CERT="ca.crt"
 
-# Generate client private key using Dilithium-5 algorithm
+# Generate server private key using Dilithium-5 algorithm
 openssl genpkey -algorithm dilithium5 -out "$CLIENT_KEY"
 
-# Generate client certificate signing request (CSR)
+# Generate server certificate signing request (CSR)
 openssl req -new -key "$CLIENT_KEY" -out "$CLIENT_CSR" -subj "/CN=Client"
 
 # Sign the CSR with the CA's private key to create the server certificate
 openssl x509 -req -in "$CLIENT_CSR" -CA "$CA_CERT" -CAkey "$CA_KEY" -CAcreateserial -out "$CLIENT_CERT" -days 365
 
-echo "Client certificate signed successfully."
+echo "Server certificate signed successfully."
--- server_cert.sh
+++ server_cert.sh
@@ -2,19 +2,19 @@
 
 
 # Set the paths
-SERVER_KEY="server.key"
-SERVER_CSR="server.csr"
-SERVER_CERT="server.crt"
-CA_KEY="ca.key"
-CA_CERT="ca.crt"
+SERVER_KEY="server_key.pem"
+SERVER_CSR="server_csr.pem"
+SERVER_CERT="server_cert.pem"
+CA_KEY="ca_key.pem"
+CA_CERT="ca_cert.pem"
 
 # Generate server private key using Dilithium-5 algorithm
-openssl genpkey -algorithm dilithium5 -out "$SERVER_KEY" 
+openssl genpkey -algorithm dilithium5 -out "$SERVER_KEY"
 
 # Generate server certificate signing request (CSR)
 openssl req -new -key "$SERVER_KEY" -out "$SERVER_CSR" -subj "/CN=Server"
 
 # Sign the CSR with the CA's private key to create the server certificate
-openssl x509 -req -in "$SERVER_CSR" -CA "$CA_CERT" -CAkey "$CA_KEY" -CAcreateserial -out "$SERVER_CERT" -days 365 
+openssl x509 -req -in "$SERVER_CSR" -CA "$CA_CERT" -CAkey "$CA_KEY" -CAcreateserial -out "$SERVER_CERT" -days 365
 
 echo "Server certificate signed successfully."
