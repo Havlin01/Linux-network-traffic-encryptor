bf233b776771f626bacf98fd04bb149784b33e77
Jan Havlin <241030@vutbr.cz>
2024-03-10 13:28:24 +0100

    test

--- server_cert.sh
+++ server_cert.sh
@@ -7,6 +7,6 @@ rm -rf "srv.key" "srv.csr" "srv.crt"
 
 openssl genpkey -algorithm dilithium3 -out srv.key
 openssl req -new -newkey dilithium3 -keyout srv.key -out srv.csr 
-openssl x509 -req -in srv.csr -out srv.crt -CA ca.crt -CAkey ca.key -CAcreateserial -days 365
+openssl x509 -req -in srv.csr -sha256 -out srv.crt -CA ca.crt -CAkey ca.key -CAcreateserial -days 365
 
 echo "Server certificate signed successfully."
