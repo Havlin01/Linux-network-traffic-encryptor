810a1cb8fd06c8a8cd031791eb2264bc4af57525
Jan Havlin <241030@vutbr.cz>
2024-03-10 17:09:22 +0100

    test

--- server_cert.sh
+++ server_cert.sh
@@ -3,10 +3,10 @@
 
 # Remove any existing server key, certificate, and certificate signing request
 
-rm -rf "srv.key" "srv.csr" "srv.crt"
+rm -rf "server-dilithium-key.pem" "server-dilithium-csr.pem " "server-dilithium-cert.pem"
 
-openssl genpkey -algorithm dilithium3 -out srv.key
-openssl req -new -newkey dilithium3 -keyout srv.key -out srv.csr 
-openssl x509 -req -in srv.csr -out srv.crt -CA ca.crt -CAkey ca.key -CAcreateserial -days 365 
+openssl genpkey -algorithm dilithium3 -out server-dilithium-key.pem
+openssl req -new -key server-dilithium-key.pem -out server-dilithium-csr.pem
+openssl x509 -req -in server-dilithium-csr.pem -out server-dilithium-cert.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365 -CAcreateserial
 
 echo "Server certificate signed successfully."
