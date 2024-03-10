514d1133a6dae60b5d7d49d47f702dc997790211
Jan Havlin <241030@vutbr.cz>
2024-03-10 23:47:01 +0100

    update

--- client_cert.sh
+++ client_cert.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-rm -rf "cli.key" "cli.csr" "cli.crt"
+rm -rf "client-dilithium-key.pem" "client-dilithium-csr.pem " "client-dilithium-cert.pem"
 
-openssl genpkey -algorithm dilithium3 -out cli.key
-openssl req -new -newkey dilithium3 -keyout cli.key -out cli.csr 
-openssl x509 -req -in cli.csr -out cli.crt -CA ca.crt -CAkey ca.key -CAcreateserial -days 365
+openssl genpkey -algorithm dilithium3 -out client-dilithium-key.pem
+openssl req -new -key client-dilithium-key.pem -out client-dilithium-csr.pem
+openssl x509 -req -in client-dilithium-csr.pem -out client-dilithium-cert.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365 -CAcreateserial
+
 
 echo "Client certificate signed successfully."
