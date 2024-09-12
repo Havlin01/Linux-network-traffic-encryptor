907517ef841f91556a87faf509f0906fdcad4f3b
Jan Havlin <241030@vutbr.cz>
2024-09-12 13:02:33 +0300

    no status read

--- encryptor_server.cpp
+++ encryptor_server.cpp
@@ -1110,7 +1110,7 @@ int main(int argc, char *argv[])
             */
 
             // Get TCP connection status
-                status = read(new_socket, bufferTCP, MAXLINE);
+                //status = read(new_socket, bufferTCP, MAXLINE);
             // Establish new hybrid key, if key_ID is recieved
             //cout << status << endl;
             if (status > 0)
